5096bb1352155b147b0f067649c326603de3f2a0
pkvac <pkvac>
2025-11-05 20:56:03 -0800

    finish everything up to deployment

--- api/server.go
+++ api/server.go
@@ -1,37 +1,61 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/pkpkvac/simplebank/db/sqlc"
+	"github.com/pkpkvac/simplebank/token"
+	"github.com/pkpkvac/simplebank/util"
 )
 
 // serves http requests for our banking service.
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
 // NewServer creates a new HTTP server and sets up routing.
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	server.router = gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
-	server.router.POST("/users", server.createUser)
+	server.setupRouter()
 
-	server.router.POST("/accounts", server.createAccount)
-	server.router.GET("/accounts/:id", server.getAccount)
-	server.router.GET("/accounts", server.listAccounts)
-	server.router.PUT("/accounts/:id", server.updateAccount)
+	return server, nil
+}
 
-	server.router.POST("/transfers", server.createTransfer)
+func (server *Server) setupRouter() {
+	router := gin.Default()
 
-	return server
+	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
+
+	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
+
+	authRoutes.POST("/accounts", server.createAccount)
+	authRoutes.GET("/accounts/:id", server.getAccount)
+	authRoutes.GET("/accounts", server.listAccounts)
+	authRoutes.PUT("/accounts/:id", server.updateAccount)
+	authRoutes.POST("/transfers", server.createTransfer)
+
+	server.router = router
 }
 
 // start runs the HTTP server on a specific address.
